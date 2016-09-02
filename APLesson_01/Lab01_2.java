53f51a6ef4ff15003a5cd0744651cd4dbf248f50
millera8465 <MillerA8465@sduhsd.lan>
2016-09-02 13:32:56 -0700

    Added Files

---
+++ APLesson_01/Lab01_2.java
@@ -0,0 +1,14 @@
+public class Lab01_2 {
+	public static void main(String[]args) {
+		System.out.println(" _");
+		System.out.println("( \\");
+		System.out.println(" ) )");
+		System.out.println("( (   .-\"\"-.  A.-.A");
+		System.out.println(" \\ \\ /      \\ / , ,  \\");
+		System.out.println("  \\   \\    =;   t   /=");
+		System.out.println("   \\    |\"\".    \',--\'");
+		System.out.println("    /  //  |  ||");
+		System.out.println("   /_,))  |_,))");
+		
+	}
+}
