68aac9dac7d52f298b81ccf86ce8dc3aac139333
millera8465 <millera8465@users.noreply.github.com>
2016-09-23 14:16:47 -0700

    Add files via upload

--- APLesson_01/Lab01_1.java
+++ APLesson_01/Lab01_1.java
@@ -1,8 +1,8 @@
 public class Lab01_1 {
 	public static void main(String[]args) {
-		System.out.println("   .----#.         .#---.");
-		System.out.println("  / \\____\\      /____/ \\ ");
-		System.out.println("  |_|_A_|       |_A_|_|");
+		System.out.println("   .---#.       .#---.");
+		System.out.println("  / \\___\\      /___/ \\ ");
+		System.out.println("  |_|_A_|      |_A_|_|");
 		System.out.println("");
 		System.out.println("No distance is too great...");
 		System.out.println("");
