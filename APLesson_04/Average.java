93a93ccc80e381b8b6a3d17f9b52b4b7de59f478
millera8465 <millera8465@users.noreply.github.com>
2016-09-29 13:08:26 -0700

    Add files via upload

--- APLesson_04/Average.java
+++ APLesson_04/Average.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class Average {
@@ -25,6 +26,6 @@ public class Average {
 		avg = (num1 + num2 + num3) / 3;
 	}
 	public static void print() {
-		System.out.printf("The average of %.5f, %.5f, and %.5f is %.5f.", num1, num2, num3, avg);
+		System.out.printf("The average of %.5f, %.5f, and %.5f is %.5f.\n", num1, num2, num3, avg);
 	}
 }
