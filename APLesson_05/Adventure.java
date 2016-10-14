06283ca073c9acee86357009ac6ce3361fc1b2be
millera8465 <millera8465@users.noreply.github.com>
2016-10-13 18:31:31 -0700

    Update Adventure.java

--- APLesson_05/Adventure.java
+++ APLesson_05/Adventure.java
@@ -28,15 +28,15 @@ public class Adventure {
 									"\nC. Go home.");
 				choice3 = keyboard.next();
 				if (choice3.equals("A")) {
-					//main choice 3 main A
+					//main end A
 					System.out.println("You fight valiently, but eventually the dragon slays you. You have died!");
 				}
-				else if (choice3.equals("B)) {
-					//main choice 3 main B
+				else if (choice3.equals("B")) {
+					//main end B
 					System.out.println("You find an old magical artifact. You are rich!");
 				}
 				else {
-					//main choice 3 main C
+					// main end C
 					System.out.println("You return safely. You aren't a hero, but at least you aren't dead!");
 				}
 			}
@@ -48,12 +48,12 @@ public class Adventure {
 				"\nB. Crawl away and treat your wounds." +
 				"\nC. Cry and wait to die.");
 				choice3 = keyboard.next();
-				if (choice3.equals(B)) {
-					//main choice 3 alt correct
+				if (choice3.equals("B")) {
+					//main alt end correct
 					System.out.println("You survive, due to your expert medical skills!");
 				}
 				else {
-					//main choice 3 alt wrong
+					//main alt end wrong
 					System.out.println("A bad decision. You have died!");
 				}
 			}
@@ -61,11 +61,49 @@ public class Adventure {
 		else {
 			//alt choice 2
 			System.out.println("A series of darts shoot out of the statue's eyes!" +
-			"\nYou are now gravely wounded. What do you do?"
+			"\nYou are now gravely wounded. What do you do?" +
 			"\nA. Try to leave the temple and return to safety" + 
 			"\nB. Call for help." +
-			"\nC. Use your first aid kit to treat your wounds." );
+			"\nC. Use your first aid kit to treat your wounds.");
 			choice2 = keyboard.next();
+			if (choice2.equals("C")) {
+				//alt choice 3 main
+				System.out.println("You have successfully been healed!" +
+						"\nWhat will you do now?" +
+						"\nA. Return home." +
+						"\nB. Continue exploring the temple." +
+						"\nC. Go back and study the mechanics of the statue.");
+				choice3 = keyboard.next();
+				if (choice3.equals("A")) {
+					//alt main end A
+					System.out.println("You have safely returned home. Be sure to go to a hospital!");
+				}
+				else if (choice3.equals("B")) {
+					//alt main end B
+					System.out.println("You have found an ancient magical artifact, making you wealthy and famous!");
+				}
+				else {
+					//alt main end C
+					System.out.println("As you study the statue, you accidentally touch a lever." +
+										"\nYou have been shot by a dart! You have died!");
+				}
+			}
+			else {
+				//alt choice 3 alt
+				System.out.println("Your atttempts at surviving on your own have proved futile. What do you do?" +
+									"\nA. Cry as the pain overwhelms you." +
+									"\nB. Throw rocks at the statue in frustration." +
+									"\nC. Treat your wounds using your first aid kit.");
+				choice3 = keyboard.next();
+				if (choice3.equals("C")) {
+					//alt alt end correct
+					System.out.println("After healing your injuries successfully, you return home with stories to tell.");
+				}
+				else {
+					//alt alt end wrong
+					System.out.println("Your failure to treat your wounds has resulted in your death.");
+				}
+			}
 		}
 	}
 }
