a838357a3edb662a76b8859e598618b97394caf1
millera8465 <millera8465@users.noreply.github.com>
2016-10-19 14:05:38 -0700

    Add files via upload

--- APLesson_05/Password.java
+++ APLesson_05/Password.java
@@ -4,18 +4,30 @@ public class Password{
 	static Scanner keyboard;
 	static String password = "CompSci";
 	static String username = "AndrewMiller";
-	public static void main(Sting[]args) {
+	public static void main(String[]args) {
 		passCheck();
 	}
 	public static void passCheck() {
 		keyboard = new Scanner(System.in);
 		System.out.println("Please enter your username:");
-		usernameentry = keyboard.nextLine();
+		String usernameentry = keyboard.nextLine();
 		System.out.println("Please enter your password:");
-		passwordentry = keyboard.nextLine();
+		String passwordentry = keyboard.nextLine();
 		
-		if (usernameentry == username && passwordentry == password) {
-			
+		if (usernameentry.equals(username) && passwordentry.equals(password)) {
+			System.out.println("You are granted access!");
+		}
+		else {
+			if (usernameentry.equals(username)) {
+				System.out.println("Your password is incorrect!");
+			}
+			else if (passwordentry.equals(password)) {
+				System.out.println("Your username is incorrect!");
+			}
+			else {
+				System.out.println("Your username and password are incorrect!");
+			}
+			passCheck();
 		}
 	}
 }
