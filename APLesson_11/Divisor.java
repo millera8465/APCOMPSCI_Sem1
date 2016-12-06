932c24bf665966c50ea7ff9c364e7ad1a682e0b3
millera8465 <millera8465@users.noreply.github.com>
2016-12-06 13:59:24 -0800

    Add files via upload

---
+++ APLesson_11/Divisor.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Divisor {
+	public static void main(String[]args) {
+		Scanner keyboard = new Scanner(System.in);
+		int[][] nums = new int[4][4];
+		
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums[i].length; j++) {
+				nums[i][j] = (int)(Math.random()*200+1);
+				System.out.print(nums[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println("Please enter a number: ");
+		int divisor = keyboard.nextInt();
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums[i].length; j++) {
+				if (nums[i][j]%divisor == 0) {
+					count++;
+				}
+			}
+		}
+		System.out.println("There are " + count + " numbers divisible by " + divisor + " in the Array.");
+	}
+}
---
+++ APLesson_11/Strings.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Strings {
+	public static void main(String[]args) {
+		Scanner keyboard = new Scanner(System.in);
+		String[][] words = new String[4][4];
+		
+		for (int i = 0; i < words.length; i++) {
+			for (int j = 0; j < words[i].length; j++) {
+				System.out.println("Please enter a word: ");
+				words[i][j] = keyboard.next();
+			}
+		}
+		
+		for (int i = 0; i < words.length; i++) {
+			for (int j = 0; j < words[i].length; j++) {
+				System.out.printf("%10s",words[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
