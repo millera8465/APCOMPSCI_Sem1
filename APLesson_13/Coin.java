6fd44d57aa4ff565937baf71e8578e97f034feec
millera8465 <millera8465@users.noreply.github.com>
2017-02-01 14:35:19 -0800

    Add files via upload

---
+++ APLesson_13/Coin.java
@@ -0,0 +1,22 @@
+public class Coin extends Money {
+	
+	private String weight;
+	
+	public Coin() {
+		super();
+		this.weight = "";
+	}
+	
+	public Coin(String n, String w, double v) { 
+		super(n, v);
+		this.weight = w;
+	}
+	
+	public String scan() {
+		return weight;
+	}
+	public String toString() {
+		return "Weight: " + weight + "\n" +
+				super.toString();
+	}
+}
---
+++ APLesson_13/ToyStore.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class ToyStore {
+	private ArrayList<Toy> toyList;
+	
+	public ToyStore() {
+		
+	}
+}
