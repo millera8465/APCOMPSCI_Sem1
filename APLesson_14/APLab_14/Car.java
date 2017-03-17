e087899fbdedcb15380d5358222118192141876f
millera8465 <millera8465@users.noreply.github.com>
2017-03-17 13:20:18 -0700

    Add files via upload

--- APLesson_14/APLab_14/Car.java
+++ APLesson_14/APLab_14/Car.java
@@ -1,15 +1,13 @@
 public class Car implements Location {
-	public int id;
+	public final int id = (int)(Math.random()*1000000);
 	public double[] location;
 	public Car() {
 		location = new double[2];
-		id = (int)(Math.random()*1000000);
 	}
 	public Car(double x, double y) {
 		location = new double [2];
 		location[0] = x;
 		location[1] = y;
-		id = (int)(Math.random()*1000000);
 	}
 	public int getID() {
 		return id;
